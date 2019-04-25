26efc91334e5ac142582d7e2cf363813d0bd2951
louieyan <iamziwan@outlook.com>
2019-04-25 11:41:06 +0800

    complete lab07 extra questions

--- lab/lab07/lab07_extra.py
+++ lab/lab07/lab07_extra.py
@@ -17,7 +17,16 @@ def remove_all(link , value):
     >>> print(l1)
     <0 1>
     """
-    "*** YOUR CODE HERE ***"
+    prev, curr = link, link.rest
+    while curr is not Link.empty:
+        if curr.first == value:
+            prev.rest, curr = curr.rest, curr.rest
+        else:
+            prev, curr = curr, curr.rest
+    
+    
+
+    
 
 # Q7
 def deep_map_mut(fn, link):
@@ -32,7 +41,14 @@ def deep_map_mut(fn, link):
     >>> print(link1)
     <9 <16> 25 36>
     """
-    "*** YOUR CODE HERE ***"
+    curr = link
+    while curr is not Link.empty:
+        if not isinstance(curr.first, Link):
+            curr.first = fn(curr.first)
+        else:
+            deep_map_mut(fn, curr.first)
+        curr = curr.rest
+    
 
 # Q8
 def has_cycle(link):
@@ -48,8 +64,20 @@ def has_cycle(link):
     >>> u = Link(2, Link(2, Link(2)))
     >>> has_cycle(u)
     False
+    >>> v = Link(1, Link(2, Link(3)))
+    >>> v.rest.rest.rest = v.rest
+    >>> has_cycle(v)
+    True
     """
-    "*** YOUR CODE HERE ***"
+    nodes_seen = []
+    curr = link
+    while curr is not Link.empty:
+        nodes_seen.append(curr)
+        curr = curr.rest
+        if curr in nodes_seen:
+            return True
+
+    return False
 
 def has_cycle_constant(link):
     """Return whether link contains a cycle.
@@ -61,8 +89,26 @@ def has_cycle_constant(link):
     >>> t = Link(1, Link(2, Link(3)))
     >>> has_cycle_constant(t)
     False
+    >>> v = Link(1, Link(2, Link(3)))
+    >>> v.rest.rest.rest = v.rest
+    >>> has_cycle_constant(v)
+    True
     """
-    "*** YOUR CODE HERE ***"
+    if link is Link.empty:
+        return False
+
+    prev, curr = link, link.rest
+    while curr is not Link.empty:
+        if curr.rest is Link.empty:
+            return False
+        elif curr.rest == prev or curr == prev:
+            return True
+        else:
+            prev, curr = prev.rest, curr.rest.rest
+    return False
+
+
+    
 
 # Q9
 def reverse_other(t):
@@ -78,4 +124,17 @@ def reverse_other(t):
     >>> t
     Tree(1, [Tree(8, [Tree(3, [Tree(5), Tree(4)]), Tree(6, [Tree(7)])]), Tree(2)])
     """
-    "*** YOUR CODE HERE ***"
+    def helper(t, need_reverse):
+        if t.is_leaf():
+            pass
+        reverse_labels = [b.label for b in t.branches][::-1]
+        for i in range(len(t.branches)):
+            b = t.branches[i]
+            if need_reverse:
+                b.label = reverse_labels[i]
+            helper(b, not need_reverse)
+    
+    helper(t, True)
+        
+    
+    
